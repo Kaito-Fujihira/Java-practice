04e115387047f8bfe973f15230e5f9e1bdec3ac6
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-23 16:22:55 +0900

    add Java-practice

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+
+class Main {
+  public static void main (String[] args) {
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+
+    Scanner scanner = new Scammer(System.in);
+
+    System.out.println("【自転車の情報】");
+    bicycle.printData();
+    System.out.println("-----------------");
+    System.out.print("走る距離を入力してください : ");
+    int distance = scanner.nextInt();
+    bicycle.run(distance);
+
+  }
+}
