aacb28b7d0e67502a39ce4e00f45b4cbf389c9fa
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-24 16:54:37 +0900

    update Car.java

--- Car.java
+++ Car.java
@@ -13,4 +13,9 @@ class Car {
     System.out.println("色 : " + this.color);
     System.out.println("走行距離 : " + this.distance + "km");
   }
+
+  public void run(int distance) {
+    System.out.println(distance + "km走ります");
+    int distance += distance; // 引数で受け取った値（走る距離）を、distanceフィールドに加算する
+    System.out.println("走行距離" + this.distance + "km");
 }
