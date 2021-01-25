4752d37d462db1ccc7d1b77ba60450d827f95c5e
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-25 21:56:32 +0900

    update Car.java

--- Car.java
+++ Car.java
@@ -15,7 +15,30 @@ class Car {
   }
 
   public void run(int distance) {
-    System.out.println(distance + "km走ります");
-    int distance += distance; // 引数で受け取った値（走る距離）を、distanceフィールドに加算する
-    System.out.println("走行距離" + this.distance + "km");
+    System.out.println(distances + "km走ります");
+    if(distances >= this.fuel) {
+      System.out.println("ガソリンが足りません");
+      System.out.println("走行距離 : " + "0km");
+      System.out.println("ガソリン量 : " + "100L");
+    }else{
+      this.distance += distances;
+      System.out.println("走行距離 : " + this.distance + "km");
+      this.fuel -= distance;
+      System.out.println("ガソリン量 : " + this.fuel + "L");
+    }
+  }
+  
+  public void charge(int fuel) {
+    System.out.println(fuel + "L給油します");
+    if(fuel <= 0) {
+      System.out.println("給油できません");
+      System.out.println("ガソリン量 : " + this.fuel + "L");
+    }else if(fuel + this.fuel >= 100){
+      System.out.println("満タンまで給油します");
+      System.out.println("ガソリン量 : " + "100L");
+    }else{
+      this.fuel += fuel;
+      System.out.println("ガソリン量 : " + this.fuel +"L");
+    }
+  }
 }
