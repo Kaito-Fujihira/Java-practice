e7f68e82f8d16345ee20c6647604eb7989ebfa2a
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-27 09:51:56 +0900

    update Main.java

--- Main.java
+++ Main.java
@@ -30,11 +30,12 @@ class Main {
 
 
     //Person.java用
-    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0, "医者");
     person1.printData();
-    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0, "教師");
     person2.printData();
     System.out.println("----------------------");
+    system.out.println("person1の仕事を" + person1.getJob() + "に変更しました");
     person1.printData();
   }
 }
