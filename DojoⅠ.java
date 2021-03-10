02fb450faed39e58b28aa1cdbb1864556ea3eb4e
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-10 13:43:39 +0900

    add int totalAge

--- DojoⅠ.java
+++ DojoⅠ.java
@@ -4,6 +4,7 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int maxAge = 0;
+    int totalAge = 0;
 
     System.out.print("何人分の情報を入力しますか：");
     int number = scanner.nextInt();
@@ -18,7 +19,9 @@ class Main {
       String lastName = scanner.next();
 
       System.out.print("年齢：");
-      int age = scanner.netInt();
+      int age = scanner.nextInt();
+
+      totalAge += age;
 
       if (age > maxAge) {
         maxAge = age;
@@ -33,5 +36,6 @@ class Main {
       Person.printData(firstName, lastName, age, height, weight)
     }
     System.out.println("最高年齢は" + maxAge + "歳です");
+    System.out.println("平均年齢は" + totalAge / number + "歳です");
   }
 }
